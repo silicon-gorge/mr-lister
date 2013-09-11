4ddc6559d745e1d94ca255bf0842fe98733c1581
micbell <michael.1.bell@nokia.com>
2013-09-11 16:01:13 +0100

    Fleshing out unit tests for the persistence namespace - work in progress

--- test/onix/unit/persistence.clj
+++ test/onix/unit/persistence.clj
@@ -1,29 +1,50 @@
 (ns onix.unit.persistence
-  (:require [onix.persistence :as persistence])
-  
-  (:use midje.sweet)
-  
-  )
+  (:require [onix.persistence :as persistence]
+            [onix.dynamolib :as dynamo])
+  (:use midje.sweet))
 
 (fact-group :unit
             
             (facts "About getting an application from store"
                    
-                   (fact "Fetching a app which does not exist returns nil")
+                   (fact "Fetching an app which does exist gets the app including its metadata"
+                         (persistence/get-application "dummy") => {:name "dummy" 
+                                                                   :metadata {:size "big" :colour "red"}}
+                         (provided 
+                           (dynamo/get-item persistence/applications-table {:hash_key "dummy"}) => {:name "dummy"
+                                                                                                    :metadata "{\"size\":\"big\",\"colour\":\"red\"}"}))
                    
-                   (future-fact "Fetching an app which does exist gets the app including its metadata")
-                   
-                   )
+                   (fact "Fetching a app which does not exist returns nil"
+                                (persistence/get-application "dummy") => nil
+                                (provided 
+                                  (dynamo/get-item persistence/applications-table {:hash_key "dummy"}) => nil)))
             
             (facts "About updating an existing application"
                    
-                   (future-fact "Adding a metadata item to an existing app completes correctly")
+                   (fact "Adding a metadata item to an existing app adds the item to the stored application."
+                                (persistence/update-application-metadata "dummy" "key" "value") => {:key "value"}
+                                (provided
+                                  (persistence/get-application "dummy") => {:name "dummy"
+                                                                            :metadata {:size "big" :colour "red"}}
+                                  (persistence/create-application {:name "dummy"
+                                                                   :metadata "{\"key\":\"value\",\"size\":\"big\",\"colour\":\"red\"}"})
+                                  => anything))
                    
-                   (future-fact "Updating a metadata item in an existing app overwrites the previous value")
+                   (fact "Updating a metadata item in an existing app overwrites the previous value"
+                                (persistence/update-application-metadata "dummy" "colour" "blue") => {:colour "blue"}
+                                (provided
+                                  (persistence/get-application "dummy") => {:name "dummy"
+                                                                            :metadata {:size "big" :colour "red"}}
+                                  (persistence/create-application {:name "dummy"
+                                                                   :metadata "{\"size\":\"big\",\"colour\":\"blue\"}"})
+                                  => anything))
                    
-                   (future-fact "Updating a metadata item in an app which doesn't exist returns nil")
-                   
-                   )
+                   (fact "Updating a metadata item in an app which doesn't exist returns nil"
+                                (persistence/update-application-metadata "dummy" "key" "value") => nil
+                                (provided
+                                  (dynamo/get-item persistence/applications-table {:hash_key "dummy"}) => nil)))
+            
+            
             
             )
 
